ee2bf7c217ed78ac10f4e8647c54e7e54ca62bb9
Valeryi Savich <Relrin78@gmail.com>
2018-04-07 18:17:49 +0300

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -6,6 +6,8 @@ app_server=${APP_SERVER:-none}
 
 if [[ ${app_server} = sanic ]]; then
     python ./manage.py run
+elif [[ ${app_server} = simplerun ]]; then
+    python run.py
 elif [[ ${app_server} = none ]]; then
     timeout=${CONTAINER_TIMEOUT:-86400}
     echo -e "Container was started in the idle mode on $timeout seconds..."
