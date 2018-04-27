5127f2e2e8c143a5d94d8be4e440f26badbd6142
Valeryi Savich <relrin78@gmail.com>
2018-04-27 21:34:01 +0300

    Added options of waiting for a service

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 mongodb_timeout=${WAIT_FOR_MONGODB:-false}
 redis_timeout=${WAIT_FOR_REDIS:-false}
 rabbitmq_timeout=${WAIT_FOR_RABBITMQ:-false}
+service_timeout=${WAIT_FOR_SERVICE:-false}
 
 
 if [[ ${mongodb_timeout} != false ]]; then
@@ -16,6 +17,10 @@ fi
 
 if [[ ${rabbitmq_timeout} != false ]]; then
     /wait-for.sh ${RABBITMQ_HOST:-rabbitmq} ${RABBITMQ_PORT:-5672} ${rabbitmq_timeout} "RabbitMQ"
+fi
+
+if [[ ${service_timeout} != false ]]; then
+    /wait-for.sh ${SERVICE_HOST:-localhost} ${SERVICE_PORT:-8000} ${service_timeout} "Service"
 fi
 
 exec "$@"
